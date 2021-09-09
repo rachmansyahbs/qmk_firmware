ab8e2ed3ef855ebedbe8c37763c8e64c9b169052
Rachmansyah Budi Setiawan <rachmansyahbs@gmail.com>
2021-09-09 21:31:35 +0800

    Added Adelie definition and Vial keymap

---
+++ keyboards/cannonkeys/adelie/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x24, 0xDF, 0xCD, 0xAC, 0xED, 0x78, 0x94, 0xB0}
+
+#define VIAL_TAP_DANCE_ENTRIES 2
+
+#define VIAL_COMBO_ENTRIES 2
